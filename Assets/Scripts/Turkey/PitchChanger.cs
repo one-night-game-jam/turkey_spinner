393313c796111431ed750d718814400c14e257e6
Shoma SATO <noir.neo.04@gmail.com>
2019-07-13 18:06:22 +0900

    Change pitch from velocity

---
+++ Assets/Scripts/Turkey/PitchChanger.cs
@@ -0,0 +1,24 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace TurkeySpinner.Turkey
+{
+    public class PitchChanger : MonoBehaviour
+    {
+        [SerializeField]
+        AudioSource audioSource;
+
+        [SerializeField]
+        Rigidbody rb;
+
+        [SerializeField]
+        float velocityPitchRate;
+
+        void Start()
+        {
+            rb.ObserveEveryValueChanged(rb => rb.angularVelocity)
+                .Subscribe(v => audioSource.pitch = v.y * velocityPitchRate)
+                .AddTo(this);
+        }
+    }
+}
