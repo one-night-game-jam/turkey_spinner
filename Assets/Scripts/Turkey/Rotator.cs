54473fa29ed9a21535c04aab668ad4b90ac67453
Shoma SATO <noir.neo.04@gmail.com>
2019-07-13 15:50:46 +0900

    Add rotator

---
+++ Assets/Scripts/Turkey/Rotator.cs
@@ -0,0 +1,29 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace TurkeySpinner.Turkey
+{
+    public class Rotator : MonoBehaviour
+    {
+        [SerializeField]
+        Rigidbody rb;
+
+        [SerializeField]
+        float initialTorque;
+
+        void Start()
+        {
+            this.FixedUpdateAsObservable()
+                .First()
+                .Subscribe(_ => AddTorque(initialTorque))
+                .AddTo(this);
+        }
+
+        void AddTorque(float y)
+        {
+            rb.angularVelocity = Vector3.zero;
+            rb.AddTorque(0, y, 0, ForceMode.Impulse);
+        }
+    }
+}
