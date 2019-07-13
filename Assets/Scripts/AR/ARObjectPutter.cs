97e58ae0f322b0d3371aa2eccb26b9712dade8e3
Shoma SATO <noir.neo.04@gmail.com>
2019-07-13 15:24:02 +0900

    Spawn Hato

---
+++ Assets/Scripts/AR/ARObjectPutter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+namespace TurkeySpinner.AR
+{
+    public class ARObjectPutter : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject prefab;
+
+        [SerializeField]
+        ARPlaneManager arPlaneManager;
+
+        void Start()
+        {
+
+            arPlaneManager.PlanesAddedAsObservable()
+                .SelectMany(x => x)
+                .Take(1)
+                .Subscribe(Put)
+                .AddTo(this);
+        }
+
+        void Put(ARPlane plane)
+        {
+            Instantiate(prefab, plane.center, Quaternion.identity);
+        }
+    }
+
+    public static class ARPlaneManagerExtensions
+    {
+        public static IObservable<List<ARPlane>> PlanesAddedAsObservable(this ARPlaneManager arPlaneManager)
+        {
+            return arPlaneManager.PlanesChangedAsObservable()
+                .Select(x => x.added)
+                .Where(x => x != null && x.Any());
+        }
+
+        public static IObservable<ARPlanesChangedEventArgs> PlanesChangedAsObservable(this ARPlaneManager arPlaneManager)
+        {
+            return Observable.FromEvent<ARPlanesChangedEventArgs>(
+                h => arPlaneManager.planesChanged += h,
+                h => arPlaneManager.planesChanged -= h);
+        }
+    }
+}
