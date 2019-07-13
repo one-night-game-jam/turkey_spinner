f36adc9121928c4cee4e5c226b2331eb43eddbc5
Shoma SATO <noir.neo.04@gmail.com>
2019-07-13 18:53:32 +0900

    Apply speed to animation

---
+++ Assets/Scripts/Turkey/AnimatorSpeedChanger.cs
@@ -0,0 +1,24 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace TurkeySpinner.Turkey
+{
+    public class AnimatorSpeedChanger : MonoBehaviour
+    {
+        [SerializeField]
+        Rigidbody rb;
+
+        [SerializeField]
+        Animator animator;
+
+        [SerializeField]
+        float velocitySpeedRate;
+
+        void Start()
+        {
+            rb.ObserveEveryValueChanged(rb => rb.angularVelocity)
+                .Subscribe(v => animator.SetFloat("Speed", v.y * velocitySpeedRate))
+                .AddTo(this);
+        }
+    }
+}
