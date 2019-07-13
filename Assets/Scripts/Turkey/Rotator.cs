0e6ebb921bffff1d55b8d466014da80e614a6403
Shoma SATO <noir.neo.04@gmail.com>
2019-07-13 16:44:41 +0900

    Swipe

--- Assets/Scripts/Turkey/Rotator.cs
+++ Assets/Scripts/Turkey/Rotator.cs
@@ -12,16 +12,29 @@ namespace TurkeySpinner.Turkey
         [SerializeField]
         float initialTorque;
 
+        [SerializeField]
+        SwipeHandler swipeHandler;
+
+        [SerializeField]
+        float swipePower;
+
         void Start()
         {
             this.FixedUpdateAsObservable()
                 .First()
                 .Subscribe(_ => AddTorque(initialTorque))
                 .AddTo(this);
+
+            swipeHandler.OnSwipeAsObservable()
+                .Select(x => x * swipePower)
+                .ObserveOn(Scheduler.MainThreadFixedUpdate)
+                .Subscribe(AddTorque)
+                .AddTo(this);
         }
 
         void AddTorque(float y)
         {
+            Debug.Log(y);
             rb.angularVelocity = Vector3.zero;
             rb.AddTorque(0, y, 0, ForceMode.Impulse);
         }
