7873c57b6c418c7acf519b0ca9bd2654aa88ff8d
Shoma SATO <noir.neo.04@gmail.com>
2019-07-14 16:59:38 +0900

    wip Record screen

---
+++ Assets/Scripts/UI/RecordButton.cs
@@ -0,0 +1,44 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.Apple.ReplayKit;
+using UnityEngine.UI;
+
+namespace TurkeySpinner.UI
+{
+    public class RecordButton : MonoBehaviour
+    {
+        [SerializeField]
+        Toggle toggle;
+
+        void Start()
+        {
+            toggle.OnValueChangedAsObservable()
+                .Skip(1)
+                .Subscribe(Changed)
+                .AddTo(this);
+
+            this.ObserveEveryValueChanged(_ => ReplayKit.isRecording)
+                .Skip(1)
+                .Subscribe(isRecording => toggle.isOn = isRecording)
+                .AddTo(this);
+        }
+
+        void OnEnable()
+        {
+            gameObject.SetActive(ReplayKit.APIAvailable);
+        }
+
+        static void Changed(bool isOn)
+        {
+            if (isOn)
+            {
+                ReplayKit.StartRecording(true);
+            }
+            else
+            {
+                ReplayKit.StopRecording();
+                ReplayKit.Preview();
+            }
+        }
+    }
+}
